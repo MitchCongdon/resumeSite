140f3d90a1ea41286b5b590711fd2d01b48064b5
Upryzing//Mitchell <mitchellcongdon@yahoo.com>
2016-06-03 10:32:03 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
-console.log("programStart")
-var myVar = setInterval(timeTicker, 1000);
+var tickTock = setInterval(timeTicker, 1000);
 
 function timeTicker() {
     var d = new Date();
     document.getElementById("timeTicker").innerHTML = d.toLocaleTimeString();
 }
-console.log("programEnd")
